f3c9713cc740f1ff3ce5c864c2a01b593b72025b
sanjaypatel01 <sanjaypatel62@gmail.com>
2023-09-01 14:25:01 +0530

    Time: 4 ms (100.00%) | Memory: 43.2 MB (19.46%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0) return false;
+        int y = x;
+        int z = 0;
+        while(y != 0) {
+            z = z * 10 + y%10;
+            y = y/10;
+        }
+        return z == x;
+    }
+}
