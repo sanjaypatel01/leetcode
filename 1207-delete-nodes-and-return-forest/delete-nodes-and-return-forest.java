85eb7188486e07a20ca9209c79b16c55279bfcb9
sanjaypatel01 <sanjaypatel62@gmail.com>
2023-09-05 12:08:00 +0530

    Time: 10 ms (5.03%) | Memory: 43.8 MB (82.07%) - LeetSync

---
+++ 1207-delete-nodes-and-return-forest/delete-nodes-and-return-forest.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+        
+        List<TreeNode> ansList = new ArrayList<>();
+        Set<Integer> toDelete = new HashSet<>();
+        for(int del: to_delete) toDelete.add(del);
+        removeNodes(root, ansList, toDelete);
+        if(!toDelete.contains(root.val)){
+            ansList.add(root);
+        }
+
+        return ansList;
+    }
+
+    private TreeNode removeNodes(TreeNode root, List<TreeNode> ansList, Set<Integer> toDelete) {
+        if(root == null) return null;
+
+        root.left = removeNodes(root.left, ansList, toDelete);
+        root.right = removeNodes(root.right, ansList, toDelete);
+        if(toDelete.contains(root.val)) {
+            System.out.println("Deleting : "+ root.val);
+            if(root.left != null) ansList.add(root.left);
+            if(root.right != null) ansList.add(root.right);
+            return null;
+        }
+        return root;
+    }
+}
