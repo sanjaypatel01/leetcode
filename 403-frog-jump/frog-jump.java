3ad7ea6dceff59cd22f268871a04bbb449697b48
sanjaypatel01 <sanjaypatel62@gmail.com>
2023-09-06 16:43:19 +0530

    Time: 128 ms (17.63%) | Memory: 89.3 MB (5.13%) - LeetSync

--- 403-frog-jump/frog-jump.java
+++ 403-frog-jump/frog-jump.java
@@ -1,16 +1,18 @@
 class Solution {
     public boolean canCross(int[] stones) {
         int n = stones.length;
-        if(n == 0) return true;
-        if(n >= 2 && stones[1] != 1) return false;
+        // Extra conditions but not really required as input length will not be less than 2.
+       if(n == 0) return true;
+       if(n >= 2 && stones[1] != 1) return false; // Even this is not required, later execution of code will handle it.
         Map<Integer, Integer> map = new HashMap<>();
         for(int i=0; i < n; i++){
             map.put(stones[i], i);
         }
-
+        // Consider assigning with max value of n and prev 
         int [][]dp = new int[2001][2001];
         for(int []arr : dp) Arrays.fill(arr, -1);
         
+        //For first stone, considering 0 as prev jump, so that it can jump 1 only as 0-1, 0-0 are not allowed.
         return canCross(0, stones, 0, map, dp);
     }
     
