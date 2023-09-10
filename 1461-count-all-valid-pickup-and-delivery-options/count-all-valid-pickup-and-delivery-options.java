87891d461dba4e53992039eb8031ea0aa4ed3cac
sanjaypatel01 <sanjaypatel62@gmail.com>
2023-09-10 21:50:38 +0530

    Time: 0 ms (100.00%) | Memory: 39.3 MB (56.71%) - LeetSync

--- 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.java
+++ 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.java
@@ -1,9 +1,10 @@
 class Solution {
     public int countOrders(int n) {
         long ans = 1;
+        int mod = (int)Math.pow(10, 9) + 7;
         int slots = 2 * n;
         while(slots > 0) {
-            ans = (ans * (slots * (slots - 1)/2))% ((int)Math.pow(10, 9) + 7);
+            ans = (ans * (slots * (slots - 1)/2))% mod;
             slots = slots - 2;
         }
         return (int)ans;
