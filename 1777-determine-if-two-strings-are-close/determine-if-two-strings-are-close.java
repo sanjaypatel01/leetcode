1a5ed52e5ffafbaa18911b1242ca5045941f461c
sanjaypatel01 <sanjaypatel62@gmail.com>
2023-09-11 10:04:05 +0530

    Time: 12 ms (81.13%) | Memory: 44.9 MB (15.41%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
@@ -3,22 +3,20 @@ class Solution {
         if(word1.length() != word2.length()) return false;
         int []count1 = new int [26];
         int []count2 = new int [26];
-        Set<Character> set1 = new HashSet<>();
-        Set<Character> set2 = new HashSet<>();
         for(int i=0; i < word1.length(); i++){
             count1[word1.charAt(i) - 'a']++;
             count2[word2.charAt(i) - 'a']++;
-            set1.add(word1.charAt(i));
-            set2.add(word2.charAt(i));
+        }
+        for(int i=0; i < count1.length; i++){
+            if(count1[i] == count2[i]) continue;
+            if(count1[i] == 0 || count2[i] == 0) return false;   
         }
         Arrays.sort(count1);
         Arrays.sort(count2);
         for(int i=0; i < count1.length; i++){
             if(count1[i] != count2[i]) return false;
         }
-        for(Character c : set1){
-            if(!set2.contains(c)) return false;
-        }
+ 
         return true;
     }
 }
