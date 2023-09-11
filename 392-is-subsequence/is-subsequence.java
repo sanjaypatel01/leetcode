4908fce3b57d366c51ca7e89f5dd159b07fd7b2c
sanjaypatel01 <sanjaypatel62@gmail.com>
2023-09-11 21:20:39 +0530

    Time: 1 ms (89.90%) | Memory: 40.4 MB (56.60%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        
+        if(s== null || s.length() == 0) return true;
+        if(t == null || t.length() == 0) return false;
+        if(s.length() > t.length()) return false;
+
+        int j = 0;
+        int matched = 0;
+        for(int i=0;i < s.length(); i++) {
+            while(j < t.length()) {
+                if(s.charAt(i) == t.charAt(j)){
+                    matched++;
+                    j++;
+                    break;
+                } 
+                j++;
+            }
+        }
+        return matched == s.length(); 
+    }
+}
