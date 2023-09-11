6f35ad2b45aed370545e0c1fd63c214ab6725d4a
sanjaypatel01 <sanjaypatel62@gmail.com>
2023-09-11 21:21:16 +0530

    Time: 1 ms (89.90%) | Memory: 40.5 MB (56.60%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -4,7 +4,6 @@ class Solution {
         if(s== null || s.length() == 0) return true;
         if(t == null || t.length() == 0) return false;
         if(s.length() > t.length()) return false;
-
         int j = 0;
         int matched = 0;
         for(int i=0;i < s.length(); i++) {
